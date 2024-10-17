f85bc9f76c04b06ccbdc4caa47fe7b489fccc54e
Nathali-Huamani28 <katyhuamani634@gmail.com>
2024-10-17 08:12:18 -0500

    quinto commit

---
+++ List.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Colegio
+{
+    public class List
+    {
+        public int Count { get; internal set; }
+
+        internal void Add(estudiante estudiante)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static implicit operator List(List<estudiante> v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
