4bfd07bbaced1edda45dcab66cbef804012a940e
Nathali-Huamani28 <katyhuamani634@gmail.com>
2024-10-17 08:11:36 -0500

    cuarto commit

---
+++ Personas.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+
+
+        namespace Personas
+{
+    public class Estudiante
+    {
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+
+        public void Estudia()
+        {
+            Console.WriteLine(Nombre + " está estudiando.");
+        }
+    }
+
+    public class Entrenador
+    {
+        public string Nombre { get; set; }
+
+        public void HaceEjercicio()
+        {
+            Console.WriteLine(Nombre + " está haciendo ejercicio.");
+        }
+    }
+
+}
+}
