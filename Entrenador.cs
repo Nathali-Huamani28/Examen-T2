052eb8aa5e660cbcad77892db58c0eab6b393872
Nathali-Huamani28 <katyhuamani634@gmail.com>
2024-10-17 08:04:56 -0500

    second commit

---
+++ Entrenador.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Colegio
+{
+    public class Entrenador
+    {
+        public string Nombre { get; internal set; }
+
+        internal void HaceEjercicio()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
